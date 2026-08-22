bf517ebf6757cedac0e2ecdb0750c005962488cd
jayhuang820 <jay.huang@solventosoft.com.tw>
2026-08-22 10:53:34 +0800

    chore: update unit tests.

--- CliUtilityServices.Tests/CommandLineInputBuilderTests.cs
+++ CliUtilityServices.Tests/CommandLineInputBuilderTests.cs
@@ -2,7 +2,7 @@ using FluentAssertions;
 
 namespace CliUtilityServices.Tests;
 
-public class CommandLineInputBuilderTests
+public partial class CommandLineInputBuilderTests
 {
     [Fact]
     public void Build_WhenCommandIsNull_ShouldThrowArgumentException()
@@ -13,5 +13,30 @@ public class CommandLineInputBuilderTests
         // Assert
         act.Should().Throw<ArgumentException>()
            .Where(p => p.ParamName == "_command");
+    }
+
+    [Fact]
+    public void WithTimeout_WhenTimeoutIsZero_ShouldThrowArgumentOutOfRangeException()
+    {
+        var builder = new CommandLineInputBuilder();
+
+        Action act = () => builder.WithTimeout(TimeSpan.Zero);
+
+        act.Should()
+            .Throw<ArgumentOutOfRangeException>()
+            .WithMessage("*Timeout must be greater than zero*");
+    }
+
+    [Fact]
+    public void WithTimeout_WhenTimeoutIsNegative_ShouldThrowArgumentOutOfRangeException()
+    {
+        var builder = new CommandLineInputBuilder();
+
+        Action act = () =>
+            builder.WithTimeout(TimeSpan.FromSeconds(-1));
+
+        act.Should()
+            .Throw<ArgumentOutOfRangeException>()
+            .WithMessage("*Timeout must be greater than zero*");
     }
 }
